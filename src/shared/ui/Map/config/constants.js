2f0206da8e33e629de20246e1f0ca042c6478010
DiDD-Developer <dimamaslov2005@vk.com>
2024-11-16 21:45:40 +0500

    feat: add individual color for each icon in map + other icons in pages

--- src/shared/ui/Map/config/constants.js
+++ src/shared/ui/Map/config/constants.js
@@ -10,10 +10,10 @@ import {
 
 export const iconsPresets = {
   1: BarIcon({ iconColor: "var(--colorRed)" }),
-  2: CinemaIcon({ iconColor: "var(--colorRed)" }),
-  3: RestIcon({ iconColor: "var(--colorRed)" }),
-  4: MusicIcon({ iconColor: "var(--colorRed)" }),
-  5: TheatreIcon({ iconColor: "var(--colorRed)" }),
-  6: CheckIcon({ iconColor: "var(--colorRed)" }),
-  7: CancelIcon({ iconColor: "var(--colorRed)" }),
+  2: CinemaIcon({ iconColor: "var(--colorMantis)" }),
+  3: RestIcon({ iconColor: "var(--colorOrangeWeb)" }),
+  4: MusicIcon({ iconColor: "var(--colorPictonBlue)" }),
+  5: TheatreIcon({ iconColor: "var(--colorVeronica)" }),
+  6: CheckIcon({ extraClasses: ["btn--isGreenLightIcon"] }),
+  7: CancelIcon({ extraClasses: ["btn--isRedIcon"] }),
 };
